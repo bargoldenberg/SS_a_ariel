f8b5e7940d0060334d6a83f2d464e2e14e650e46
bargoldenberg <barg991@gmail.com>
2021-11-02 15:29:17 +0200

    Main Fix

--- main.c
+++ main.c
@@ -5,29 +5,28 @@ int a;
 int b;
 scanf("%d",&a);
 scanf("%d",&b);
-printf("\n");
-printf("1 The Armstrong Numbers are: ");
+printf("The Armstrong Numbers are: ");
 for(int i =a;i<=b;i++){
     if(isArmstrong(i)){
         printf("%d ",i);
     }
 }
 printf("\n");
-    printf("2 The Palindroms are: ");
+    printf("The Palindroms are: ");
 for(int i =a;i<=b;i++){
     if(isPalindrome(i)){
         printf("%d ",i);
     }
 }
 printf("\n");
-    printf("3 The Prime Numbers are: ");
+    printf("The Prime Numbers are: ");
 for(int i =a;i<=b;i++){
     if(isPrime(i)){
         printf("%d ",i);
     }
 }
 printf("\n");
-    printf("4 The Strong Numbers are: ");
+    printf("The Strong Numbers are: ");
 for(int i =a;i<=b;i++){
     if(isStrong(i)){
         printf("%d ",i);
