72d20c3362afb7b2f81989397a1970bfdb8fb6f5
bargoldenberg <barg991@gmail.com>
2021-11-04 18:01:52 +0200

    Added documentation

--- advancedClassificationLoop.c
+++ advancedClassificationLoop.c
@@ -1,5 +1,8 @@
 #include<math.h>
 #include"NumClass.h"
+/*This function uses a for loop to sum all digits in a given number, each digit will be
+ put to the power of the amount of digits, it is done by using modulo and divition opererators on integers.
+ */
 int isArmstrong(int a){
 if(a<0){
 return 0;
@@ -15,6 +18,10 @@ for(int i =0;i<digits;i++){
 return(original==ans);
 
 }
+/*In this function we put each digit of the number into an array, then we assign two pointers to 
+the first element and the last element of the array. we check if they are equal and continue in the loop.
+if not, return false.
+*/
 int isPalindrome(int a){
 if(a<0){
 return 0;
--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,6 +1,9 @@
 #include<math.h>
 #include<stdio.h>
 #include"NumClass.h"
+/*helper function that uses recursion to sum all digits in a given number, each digit will be
+ put to the power of the amount of digits, it is done by using modulo and divition opererators on integers.
+ */
 int isarmstrongrec(int a,int length){
 if(a<0){
 return 0;
@@ -12,10 +15,16 @@ return 0;
         return pow(a%10,length)+isarmstrongrec(a/10,length);
     }
 }
+/*main function that calls helper function.
+*/
 int isArmstrong(int a){
     int length= (int)log10(a)+1;
     return a==isarmstrongrec(a,length);
 }
+/*In this function we put each digit of the number into an array, then we assign two pointers to 
+the first element and the last element of the array. we check if they are equal and continue in the recursion.
+if not, return false.
+*/
 int ispalirec(char A[],int i,int j){
  if(i==j||(i==j-1&&A[i]==A[j])){
      return 1;
